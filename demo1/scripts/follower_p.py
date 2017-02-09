f34026d5db404bbc14d88be4494edb359281611e
ne-v0y <voyageryifan@gmail.com>
2017-02-09 10:54:31 -0700

    added second camera

--- demo1/scripts/follower_p.py
+++ demo1/scripts/follower_p.py
@@ -14,31 +14,34 @@ class Follower:
   def __init__(self):
     cv2.namedWindow("window", 1)
     self.srv = Server(lineColorConfig, self.cfg_callback)
-    self.go = True
+    self.go = False
+    self.stop = False
     self.bridge = cv_bridge.CvBridge()
     self.image_sub = rospy.Subscriber('camera/rgb/image_raw',
                                       Image, self.image_callback)
     self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop',
-                                       Twist, queue_size=1)
+                                       Twist, queue_size=5)
     self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
     self.xtwists = []
     self.ztwists = []
-    
+
     self.err_pred = []
     self.err_past = []
 
     self.lower_yellow = np.array([ 10,  10,  10])
     self.upper_yellow = np.array([255, 255, 250])
 
-    self.lower_blue = 50
-    self.lower_green = 50
-    self.lower_red = 72
-    self.upper_blue = 180
-    self.upper_green = 175
-    self.upper_red = 163
+    self.lower_blue = 79
+    self.lower_green = 150
+    self.lower_red = 0
+    self.upper_blue = 225
+    self.upper_green = 255
+    self.upper_red = 101
 
     self.lowerbound_blue = np.array([self.lower_blue, self.lower_green, self.lower_red])
     self.upperbound_blue = np.array([self.upper_blue, self.upper_green, self.upper_red])
+
+    self.web_cap = cv2.VideoCapture(0)
 
   def cfg_callback(self, data, level):
       self.upper_red = data['upper_red']
@@ -56,22 +59,58 @@ class Follower:
   def joy_callback(self, data):
     if data.buttons[0]:
       self.go = not self.go
+      #if not self.go:
+       #self.stop = True
       self.xtwists = []
       self.ztwists = []
 
+  def webcam(self):
+    while(True):
+      ret, frame = self.web_cap.read()
+      if ret:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(hsv, (0, 70, 50), (10, 255, 180))
+        mask2 = cv2.inRange(hsv, (170, 70, 50), (180, 255, 255))
+        mask = mask1 | mask2
+        mask = cv2.erode(mask, np.ones((5, 5), np.uint8), iterations=1)
+        mask = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=1)
+        img, cts, hir = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(cts) > 0:
+            rect = list(cv2.boundingRect(cts[0]))
+            #print rect
+            '''
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            print box
+            '''
+            cv2.rectangle(frame, (rect[0], rect[1]),(rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 2)
+            #print len(retval)
+            #cv2.drawContours(frame, [box], 0, (0,0,255), 2)
+            if rect[2] * rect[3] > 5:
+                self.stop = True
+        else:
+            self.stop = False
+        cv2.imshow('webcam', frame)
+
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    self.web_cap.release()
+    cv2.destroyAllWindows()
+
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+    #image = cv2.flip(image, flipCode = 0)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    #mask = cv2.inRange(hsv, self.lower_yellow, self.upper_yellow)
 
-    print self.lowerbound_blue, self.upperbound_blue
+    #print self.lowerbound_blue, self.upperbound_blue
     mask = cv2.inRange(hsv, self.lowerbound_blue, self.upperbound_blue)
     #mask = cv2.erode(mask, np.ones((5, 5), np.uint8), iterations=1)
     #mask = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=1)
 
     # larger search area
     h, w, d = image.shape
-    search_top = h/5 * 3
+    search_top = h / 4 * 3
     search_bot = h
     mask[0:search_top, 0:w] = 0
     mask[search_bot:h, 0:w] = 0
@@ -84,35 +123,36 @@ class Follower:
       M02_ = int(M['m02']) - pow(int(M['m01']),2) / int(M['m00'])
       M20_ = int(M['m20']) - pow(int(M['m10']),2) / int(M['m00'])
       M11_ = int(M['m11']) - int(M['m10']) * int(M['m01']) / int(M['m00'])
-      tan2th = 2 * M11_ / (M20_ - M02_)
-      theta = math.degrees(math.atan(tan2th)/2)
-      rospy.loginfo("degree is "+ str(theta) )
+      theta = 0
+      if not M20_ - M02_ == 0:
+        tan2th = 2 * M11_ / (M20_ - M02_)
+        theta = math.degrees(math.atan(tan2th)/2)
+        rospy.loginfo("degree is "+ str(theta) )
       # BEGIN CONTROL
       err = cx - w/2
       self.err_past.append(err)
-      
-      
-      
-      
-      x = 0.1
-      z = -float(err) / 100 #+ (theta - 45 ) / 100
-      self.xtwists.append(x)
-      self.ztwists.append(z)
-      
-      if self.go:
-        if len(self.xtwists) > 20:
+
+      x = 0.3
+      z = -float(err) / 100 #+ (theta - 45 ) / 100)
+
+      print self.go
+      if self.go and (not self.stop):
+        self.xtwists.append(x)
+        self.ztwists.append(z)
+
+        if len(self.xtwists) > 7:
           twist = Twist()
           twist.linear.x = np.mean(self.xtwists)
           twist.angular.z = np.mean(self.ztwists)
           self.cmd_vel_pub.publish(twist)
           self.xtwists.pop(0)
           self.ztwists.pop(0)
-        
       # END CONTROL
     cv2.imshow("window", mask)
     cv2.waitKey(3)
 
 rospy.init_node('follower')
 follower = Follower()
+follower.webcam()
 rospy.spin()
 # END ALL
