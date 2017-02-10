43fc572bebdf358efd000a816bc8dc75caa5723f
Martin Humphreys <n17r4m@gmail.com>
2017-02-09 17:31:28 -0700

    added env

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/martin/Documents/Classes/Cmput412/src
+# Build directory: /home/martin/Documents/Classes/Cmput412/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(followbot)
