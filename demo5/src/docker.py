8fcdb38e719400a3382fbfc18433ce5b34c41ce8
ne-v0y <voyageryifan@gmail.com>
2017-03-08 20:11:35 -0700

    fix findChessboard return

--- demo5/src/docker.py
+++ demo5/src/docker.py
@@ -7,6 +7,7 @@
 import rospy, cv2, cv_bridge
 import numpy as np
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Twist
 
 class Docker:
@@ -17,36 +18,38 @@ class Docker:
         self.twist = Twist()
         self.eye = np.identity(3)
         self.axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-        self.objp = np.zeros((6*7,3), np.float32)
-        self.objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+        self.objp = np.zeros((6*8,3), np.float32)
+        self.objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
         self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    
+
     def img_cb(self, msg):
+        #np_arr = np.fromstring(msg.data, np.uint8)
+        #img_np = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
         img  = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #corners = np.zeros((8,6))
         ret = cv2.findChessboardCorners(gray, (8,6), None)
-        
+
         if ret == True:
+            print "found it"
             corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1), self.criteria)
             z = np.array([0.0,0.0,0.0,0.0,0.0])
             # Find the rotation and translation vectors.
             rvecs, tvecs, inliers = cv2.solvePnPRansac(self.objp, corners2, self.eye, z)
-    
+
             # project 3D points to image plane
             imgpts, jac = cv2.projectPoints(self.axis, rvecs, tvecs, self.eye, z)
-    
-            img = self.draw(img,corners2,imgpts)
+
+            img = self.draw(gray,corners2,imgpts)
             cv2.imshow('img',img)
             k = cv2.waitKey(1) & 0xff
             self.navi(0, 0) # todo
         else:
             cv2.imshow('img',img)
             k = cv2.waitKey(1) & 0xff
-    
+
     def navi(self, dist, theta):
         pass
-        
+
     def draw(self, img, corners, imgpts):
         corner = tuple(corners[0].ravel())
         img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
