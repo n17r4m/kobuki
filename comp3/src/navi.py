1f834eef0fba1e55e39e4f32f30714ad66850fa3
ne-v0y <voyageryifan@gmail.com>
2017-03-15 15:41:24 -0600

    corrected positions

--- comp3/src/navi.py
+++ comp3/src/navi.py
@@ -2,7 +2,7 @@
 
 # author: Noni
 # date: Mar 14th
-# reference: 
+# reference:
 
 # navi on a map with four distinguished goals, and loop three times
 
@@ -11,15 +11,16 @@ import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import Pose
+import math
 
 goals = [
-    [(-0.95, -9.67, 0.0), (0.0, 0.0, -0.61, 0.79)], # top left corner
-    [( 0.22, -9.67, 0.0), (0.0, 0.0,  0.01, 1.00)], # bottom left
-    [( 0.14, -5.86, 0.0), (0.0, 0.0, -0.99, 0.14)], # bottom right
-    [(-1.57, -6.52, 0.0), (0.0, 0.0, -0.99, 0.19)]  # top right
+    [(-0.45, -12.6, 0.0), (0.0, 0.0,  0.00, 1.00)], # top left corner
+    [( 2.00, -12.2, 0.0), (0.0, 0.0,  0.66, 0.75)], # bottom left
+    [( 0.64, -6.00, 0.0), (0.0, 0.0, -1.00, 0.00)], # bottom right
+    [(-1.93, -6.73, 0.0), (0.0, 0.0, -0.68, 0.74)]  # top right
     ]
 
-def goal_pose(pose): 
+def goal_pose(pose):
     goal_pose = MoveBaseGoal()
     goal_pose.target_pose.header.frame_id = 'map'
     goal_pose.target_pose.header.stamp = rospy.Time.now()
@@ -32,33 +33,44 @@ def goal_pose(pose):
     goal_pose.target_pose.pose.orientation.w = pose[1][3]
 
     return goal_pose
-    
-position = (0,0)
+
+position = [0,0]
 def amcl_cb(pose):
     global position
-    position[0] = pose.position.x
-    position[1] = pose.position.y
+    position[0] = pose.pose.pose.position.x
+    position[1] = pose.pose.pose.position.y
 
 def close_to_goal(goal):
     x, y = goal.target_pose.pose.position.x, goal.target_pose.pose.position.y
-    return math.sqrt((x - position[0])**2 + (y - position[1])**2) < 0.05
-    
+    d = math.sqrt((x - position[0])**2 + (y - position[1])**2)
+    print "Distance to goal:", d
+    return d < 0.5
+
 if __name__ == '__main__':
     rospy.init_node('patrol')
-    rospy.Subscriber('/amcl_pose', Pose, amcl_cb)
+    rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, amcl_cb)
+    rate = rospy.Rate(10)
 
-    client = actionlib.SimpleActionClient('move_base', MoveBaseAction) 
-    client.wait_for_server()
-    
-    loop = 0
-    while loop < 3:
-        for pose in goals: 
-            notNear = True
-            while notNear:
-                goal = goal_pose(pose)
-                client.send_goal(goal)
-                client.wait_for_result(rospy.Duration.from_sec(0.5))
-                if close_to_goal(goal):
-                    notNear = False
-        
-        loop += 1
+    try:
+        while not rospy.is_shutdown():
+
+            client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+            client.wait_for_server()
+
+            loop = 0
+            while loop < 3:
+                for pose in goals:
+                    print "New goal:", pose
+                    notNear = True
+                    while notNear:
+                        goal = goal_pose(pose)
+                        client.send_goal(goal)
+                        client.wait_for_result(rospy.Duration.from_sec(0.5))
+                        if close_to_goal(goal):
+                            notNear = False
+
+                loop += 1
+
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
