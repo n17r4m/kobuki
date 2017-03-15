1c8826915122f862b2831e2228293e4c37d1cc5d
ne-v0y <voyageryifan@gmail.com>
2017-03-15 17:14:09 -0600

    reverse to using while loop

--- comp3/src/navi.py
+++ comp3/src/navi.py
@@ -36,33 +36,16 @@ def goal_pose(pose):
     return goal_pose
 
 position = [0,0]
-goal = 0
-loops = 0
-
-def next_goal():
-    global goal;
-    goal = (goal + 1) % 4
-    if goal is 0:
-        loops += 1
-
 def amcl_cb(pose):
-    global go_g, position, goal, loops, client
+    global position
     position[0] = pose.pose.pose.position.x
     position[1] = pose.pose.pose.position.y
-    pose = goal_pose(goals[goal]) # get once
-    if close_to_goal(goal) and loops <= 3:
-        next_goal()
-    pose = goal_pose(goals[goal]) # get again (in case changed)
-    if go_g:
-        client.send_goal(goal)
-        client.wait_for_result(rospy.Duration.from_sec(0.5))
-
 
 def close_to_goal(goal):
     x, y = goal.target_pose.pose.position.x, goal.target_pose.pose.position.y
     d = math.sqrt((x - position[0])**2 + (y - position[1])**2)
     print "Distance to goal:", d
-    return d < 0.25
+    return d < 0.5
 
 go_g = False
 def joy_cb(msg):
@@ -74,9 +57,30 @@ if __name__ == '__main__':
     rospy.init_node('patrol')
     rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, amcl_cb)
     rospy.Subscriber('/joy', Joy, joy_cb)
-    
-    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-    client.wait_for_server()
-    
-    rospy.spin()
-    
+    rate = rospy.Rate(10)
+
+    try:
+
+        #if go_g:
+        client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        client.wait_for_server()
+
+        loop = 0
+        while loop < 3:
+            for pose in goals:
+                print "New goal:", pose
+                notNear = True
+                while notNear:
+                    rospy.spin()
+                    #if not rospy.is_shutdown():
+                    goal = goal_pose(pose)
+                    client.send_goal(goal)
+                    client.wait_for_result(rospy.Duration.from_sec(0.25))
+                    if close_to_goal(goal):
+                        notNear = False
+
+            loop += 1
+
+
+    except rospy.ROSInterruptException:
+        pass
