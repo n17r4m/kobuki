3b9bd368040e18d1c758499e610a18950f6cd8e2
Martin <n17r4m@gmail.com>
2017-03-24 03:33:42 +0000

    part 3

--- demo6/src/part3.py
+++ demo6/src/part3.py
@@ -105,7 +105,8 @@ class Part3:
             
             dst2 = dst_pts[matchesMask].reshape(dst_pts.shape[0], 2)
             src2 = src_pts[matchesMask].reshape(dst_pts.shape[0], 2)
-            src2 = np.append(src2, 0, axis=1)
+            src2 = np.append(src2, [0], axis=1)
+            
             
             #pnp = cv2.solvePnPRansac(rect3d, rect, self.K, self.D)
             pnp = cv2.solvePnPRansac(src2, dst2, self.K, self.D)
