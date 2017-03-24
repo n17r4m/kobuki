a78169923772d0b05c7e14d81b5a8a037f027a68
Martin <n17r4m@gmail.com>
2017-03-24 02:30:06 +0000

    part 3

--- demo6/src/part3.py
+++ demo6/src/part3.py
@@ -139,19 +139,20 @@ class Part3:
 
 
     def draw(self, img, corners, imgpts):
-        """
-        offset = np.array([self.target_image.shape[0], 0])
+        
+        offset = np.array([self.target_image.shape[0]/2, 0])
         mid = tuple(np.array([np.mean(corners[:,:,0]), np.mean(corners[:,:,1])]).astype(int) + offset)
         img = cv2.line(img, mid, tuple((imgpts[0] + offset).astype(int).ravel()), (255,0,0), 5)
         img = cv2.line(img, mid, tuple((imgpts[1] + offset).astype(int).ravel()), (0,255,0), 5)
         img = cv2.line(img, mid, tuple((imgpts[2] + offset).astype(int).ravel()), (0,0,255), 5)
-        """
         
+        """
         mid = tuple(np.array([np.mean(corners[:,:,0]), np.mean(corners[:,:,1])]).astype(int))
         img = cv2.line(img, mid, tuple((imgpts[0]).astype(int).ravel()), (255,0,0), 5)
         img = cv2.line(img, mid, tuple((imgpts[1]).astype(int).ravel()), (0,255,0), 5)
         img = cv2.line(img, mid, tuple((imgpts[2]).astype(int).ravel()), (0,0,255), 5)
         return img
+        """
 
 
 if __name__ == "__main__":
