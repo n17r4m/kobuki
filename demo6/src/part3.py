604f6fa1d47f8b20193a168350488730a15f4471
Martin <n17r4m@gmail.com>
2017-03-24 02:57:06 +0000

    part 3

--- demo6/src/part3.py
+++ demo6/src/part3.py
@@ -125,6 +125,7 @@ class Part3:
             matchesMask = None
             rect = np.zeros((4, 1, 2), dtype=np.int)
             imgpts = np.zeros((3, 1, 2), dtype=np.int)
+            imgpts2 = imgpts
             
         
         draw_params = dict(matchColor = (0,255,0), # draw matches in green color
@@ -143,7 +144,7 @@ class Part3:
 
     def draw(self, img, imgpts, imgpts2):
         
-        offset = np.array([0,0]) # np.array([self.target_image.shape[0], self.target_image.shape[1]/2])
+        offset = np.array([self.target_image.shape[0], 0]) # self.target_image.shape[1]/2])
         #mid = tuple(np.array([np.mean(corners[:,:,0]), np.mean(corners[:,:,1])]).astype(int) + offset)
         img = cv2.line(img, tuple((imgpts[0] + offset).astype(int).ravel()), tuple((imgpts2[0] + offset).astype(int).ravel()), (255,0,0), 5)
         img = cv2.line(img, tuple((imgpts[1] + offset).astype(int).ravel()), tuple((imgpts2[1] + offset).astype(int).ravel()), (0,255,0), 5)
